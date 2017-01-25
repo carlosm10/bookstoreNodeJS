d2a953247cb92296bd46a1a4734ec7e0f0fbfb24
Carlos Mondragon <carlosmondragon10@gmail.com>
2017-01-25 17:10:40 -0600

    First commit

---
+++ app.js
@@ -0,0 +1,126 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+//Initialize bodyparser
+app.use(bodyParser.json());
+
+//Create objects from models
+Genre = require('./models/genre'); 
+Book = require('./models/book'); 
+
+//setup mongodb connection
+mongoose.connect('mongodb://localhost/bookstore');
+var db = mongoose.connection;
+
+//Root
+app.get('/', function(req, res){
+	res.send('Please use /api/books or /api/genres');
+});
+
+//Genres functions
+
+//Get Genres
+app.get('/api/genres', function(req,res){
+	Genre.getGenres(function(err, genres){
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
+//Add Genres
+app.post('/api/genres', function(req,res){
+	var genre = req.body;
+	Genre.addGenre(genre, function(err, genre){
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+//Update Genres
+app.put('/api/genres/:_id', function(req,res){
+	var id = req.params._id;
+	var genre = req.body;
+	Genre.updateGenre(id, genre, {}, function(err, genre){
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+//Remove Genres
+app.delete('/api/genres/:_id', function(req,res){
+	var id = req.params._id;
+	Genre.deleteGenre(id, function(err, genre){
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+
+//Books Functions
+//Get Books
+app.get('/api/books', function(req,res){
+	Book.getBooks(function(err, books){
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+//Get Books by Id
+app.get('/api/books/:_id', function(req,res){
+	Book.getBookById(req.params._id, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+//Add Books
+app.post('/api/books', function(req,res){
+	var book = req.body;
+	Book.addBook(book, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+//Update Books
+app.put('/api/books/:_id', function(req,res){
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+//Remove Books
+app.delete('/api/books/:_id', function(req,res){
+	var id = req.params._id;
+	Book.deleteBook(id, function(err, book){
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+//Set Port
+app.listen(3000);
+console.log('Running on port 3000...');
